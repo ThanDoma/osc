fb87a1b6cfd2717f175b60e76b12f8201eadb6e9
ThanDoma <42151643+ThanDoma@users.noreply.github.com>
2023-01-17 15:53:21 +0300

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 import osc as te
 import numpy as np
 
@@ -7,36 +8,53 @@ class Example(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
-        self.parent.title("Окно по центру экрана")
+        self.parent.title("")
         self.pack(fill=BOTH, expand=1)
-        self.centerWindow()
+        #self.centerWindow()
         global lbl
         lbl = Label(root, width = 100, text="")
         lbl.pack()
         super().__init__()
         self.initUI()
         
- 
-    def centerWindow(self):
-        w = 290
-        h = 150
- 
-        sw = self.parent.winfo_screenwidth()
-        sh = self.parent.winfo_screenheight()
- 
-        x = (sw - w) / 2
-        y = (sh - h) / 2
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
     
 
     def initUI(self):
-      frame = Frame(self, relief=RAISED, borderwidth=1)
+      frame = Frame(
+      root, #Обязательный параметр, который указывает окно для размещения Frame.
+      padx = 10, #Задаём отступ по горизонтали.
+      pady = 10 #Задаём отступ по вертикали.
+      )
+      
       frame.pack(fill=BOTH, expand=True)
+      
+      height_lb = Label(
+      frame,
+      text="Введите кодовую последовательность "
+      )
+      height_lb.grid(row=3, column=1)
+      
+      weight_lb = Label(
+      frame,
+      text="Коэф. скругления формируещего фильтра  ",
+      )
+      weight_lb.grid(row=4, column=1)
+      
+      BIN_tf = Entry(
+      frame, #Используем нашу заготовку с настроенными отступами.
+      )
+      BIN_tf.grid(row=3, column=2)
+      
+      RC_tf = Entry(
+      frame,
+      )
+      RC_tf.grid(row=4, column=2, pady=5)
       
       def caption(event):
           global t
           alist = [0,1]
-          t = ent.get()
+          t = BIN_tf.get()
+          r = float(RC_tf.get())
           l = len(t)
           fix = l%4
 
@@ -56,17 +74,16 @@ class Example(Frame):
             for i in range(l):
                 a.append(int(t[i]))
             lbl['text'] = ''
-            te.osc(a)
+            te.osc(a, r)
           elif all(check()) and l%4!=0: lbl['text'] = f'ОШИБКА: не хватает {4-fix} символа(ов)'
           
 
 
       self.pack(fill=BOTH, expand=True)
-      global ent
       
-      ent = Entry(root, width = 100)
-      ent.pack(expand=1)
-      ent.bind('<Return>', caption)
+      BIN_tf.bind('<Return>', caption)
+      RC_tf.bind('<Return>', caption)
+      
       root.bind('<Control-z>', exit_)
 
 def exit_(event):
@@ -75,6 +92,7 @@ def exit_(event):
 def main():
   global root
   root = Tk()
+  root.geometry('400x300')
   ex = Example(root)
   root.bind('<Control-z>', exit_)
   root.mainloop()
